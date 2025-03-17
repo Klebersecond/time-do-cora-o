015df0c78549e613d4686804935ccb223b76a116
LAB53 <lab53@fatec.com>
2025-03-17 09:27:30 -0300

    time do coração

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+
+
+Console.Write("San");
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.Write("tos");
+
+Console.ResetColor();
